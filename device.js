9a3900b7584c7fc06b227cdd0afac6c34ec8105b
Cole Gleason <colewashere@gmail.com>
2014-08-03 08:29:41 -0700

    device: add color mixing

--- device.js
+++ device.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
 var path = require('path');
-
+var rybColorMixer = require('ryb-color-mixer');
 var app = require('./app');
 
 function Device(data) {
@@ -17,10 +17,13 @@ function Device(data) {
         app.store.update(Device)(this.name, data, cb);
     };
 
+    this.normalize = function(states, cb) {
+        return cb(rybColorMixer.mix(states, {result: 'rgb'}));
+    };
+
     return this;
 }
 Device.path = 'devices';
 
 module.exports.get = Device.prototype.get = app.store.get(Device);
-module.exports.update = Device.prototype.update = app.store.update(Device);
 module.exports.on = Device.prototype.on = app.store.on(Device);
